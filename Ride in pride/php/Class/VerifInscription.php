ee359af273738ac4b44618d72f712c61ff807457
Arthur Hallier <tutur.77@hotmail.fr>
2019-02-13 16:37:09 +0100

    changes

--- Ride in pride/php/Class/VerifInscription.php
+++ Ride in pride/php/Class/VerifInscription.php
@@ -1,18 +1,34 @@
 <?php
 
 namespace App;
-
 use \PDO;
 
-class VerifInscription {
-    private $login;
-    private $password;
-    public function __construct($login,$password) {
-        $this->login  = $login;
-        $this->password = $password;
+require 'Autoloader.php';
+Autoloader::register();
 
+
+
+
+
+if  (
+    htmlspecialchars(isset($_POST['email'])) &&
+    htmlspecialchars(isset($_POST['password'])) &&
+    htmlspecialchars(!empty($_POST['email'])) &&
+    htmlspecialchars(!empty($_POST['password']))
+    )
+    {
+
+        class VerifInscription {
+            private $email;
+            private $password;
+
+            public function __construct($email,$password) {
+                $this->email  = $email;
+                $this->password = $password;
+
+            }
+
+        }
     }
-
-}
 
 ?>

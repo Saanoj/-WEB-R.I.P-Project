2fefa51d518b9507336bd4d1c4a8721eecd04983
Jonas <jonasnizard@gmail.com>
2019-02-17 21:38:40 +0100

    navbar

--- navbar/chooseNav.php
+++ navbar/chooseNav.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 
 $bdd = new PDO('mysql:host=localhost;dbname=rip', 'root', '');
 
@@ -7,18 +7,6 @@ if(isset($_SESSION['id'])){
       $query=$bdd->prepare('SELECT isAdmin
       FROM users WHERE id = :id');
       $query->bindValue(':id',$_SESSION['id']);
-=======
-namespace App;
-use \PDO;
-
-require '../Class/Autoloader.php';
-Autoloader::register();
-$bdd = new Database('rip');
-if(isset($_SESSION['id'])){
-      $query=$bdd->prepare('SELECT isAdmin
-      FROM USER WHERE id = :id');
-      $query->bindValue(':id',$_SESSION['id'], PDO::PARAM_STR);
->>>>>>> faba875b73e192ec3963a804f1f383813391e774
       $query->execute();
       $data=$query->fetch();
       if($data['isAdmin'] == 1){

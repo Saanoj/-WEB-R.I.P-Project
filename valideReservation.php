f73e5e33bd934e125c9d3dd751be9de63ec310ea
Tutur77 <38217644+Tutur77@users.noreply.github.com>
2019-02-26 22:00:02 +0100

    Date du trajet doit etre supérieur a la date en cours et pareil pour les heures/.minutes si date trajet =  date en cours

--- valideReservation.php
+++ valideReservation.php
@@ -5,15 +5,25 @@ App\Autoloader::register();
 $bdd = new App\Database('rip');
 
 // VERIFICATION DE LA DATE DU TRAJET : IL DOIT ETRE SUPERIEUR A LA DATE DU JOUR 
-
-$date1 = new DateTime($_POST['dateDebut']);
-$date2 = new DateTime("now");
-var_dump($date1 < $date2);
-
-
-
 $timeStart = $_POST["dateDebut"]." ".$_POST["heureDebut"];
-echo $timeStart;
+$dateTrajet = new DateTime($timeStart, new DateTimeZone('Europe/Paris'));
+$dateNow = new DateTime("now", new DateTimeZone('Europe/Paris'));
+$interval = $dateTrajet->diff($dateNow);
+var_dump($interval->format('%Y %M %D %H %i %S %R'));
+var_dump($interval->format('%S') > 0);
+var_dump($interval->format('%i') > 0);
+var_dump($interval->format('%H') > 0);
+var_dump($interval->format('%D') > 0);
+var_dump($interval->format('%M') > 0);
+var_dump($interval->format('%Y') > 0);
+var_dump($interval->format('%R'));
+var_dump($interval->format('%R') == "-");
+if ($interval->format('%R') == "-")
+{
+  if($interval->format('%i') > 0 || $interval->format('%S') > 0 || $interval->format('%H') > 0 || $interval->format('%D') > 0|| $interval->format('%M') > 0 || $interval->format('%Y') > 0)
+  {
+  
+// echo $timeStart;
 $timestampStart = strtotime($timeStart);
 // echo "<br>".$timestampStart;
 
@@ -22,8 +32,8 @@ $end = str_replace(' ',"%20",$_POST["end"]);
 $start = str_replace(',',"",$start);
 $end = str_replace(',',"",$end);
 
-echo "<br>".$start;
-echo "<br>".$end;
+//echo "<br>".$start;
+//echo "<br>".$end;
 
 $apiReturn = App\Trajet::getDistanceTime($start, $end);
 
@@ -34,23 +44,42 @@ $dateFin = gmdate('Y-m-d G:i:s', $timestampEnd);
 $trajet = new App\Trajet($_POST["start"],$_POST["end"],0,$_SESSION['id'],date('Y-m-d G:i:s'),$timeStart,$dateFin,$apiReturn["distance"],$apiReturn["time"]);
 $trajet->addTrajetStart($bdd,'INSERT INTO `trajet` (`idClient`, `debut`, `fin`, `prixTrajet`, `heureDebut`,`heureFin`,`dateResevation`,`distanceTrajet`) VALUES (:idClient,:debut,:fin,:prixTrajet,:dateDebut,:dateFin,:dateReservation,:distanceTrajet)',$trajet);
 $trajet->startSessionId($bdd); //add idTrajet in SESSION
-echo "<br> temps trajet: ".$trajet->getTimeTrajet();
+// echo "<br> temps trajet: ".$trajet->getTimeTrajet();
 
-echo "<br>";
-echo "id trajet: ".$_SESSION["idTrajet"];
-echo "<br>";
-echo "id: ".$_SESSION["id"];
+//echo "<br>";
+//echo "id trajet: ".$_SESSION["idTrajet"];
+//echo "<br>";
+//echo "id: ".$_SESSION["id"];
 
 
 $_SESSION['trajet'] = serialize($trajet);
 
 $data=$bdd->query('SELECT * FROM abonnement WHERE idClient='.$_SESSION['id'].'');
 if (!empty($data)) {
-  echo "found";
+ // echo "found";
 
- // header("location: resevationChooseService.php");
+  header("location: resevationChooseService.php");
 }else{
-  echo "not found";
-//  header("location: resevationChooseDriver.php");
+ // echo "not found";
+   header("location: resevationChooseDriver.php");
 }
- ?>
+
+
+ 
+
+  }
+  else {
+    header('location:ReservationTrajet.php');
+  }
+}
+else {
+  header('location:ReservationTrajet.php');
+  
+}
+
+
+
+
+
+
+

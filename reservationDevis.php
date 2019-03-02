a932ae8e241eacce8326719a6301ca215e1bb66f
natin56y <32836662+natin56y@users.noreply.github.com>
2019-03-02 18:31:01 +0100

    changements mineurs presentation

--- reservationDevis.php
+++ reservationDevis.php
@@ -1,16 +1,16 @@
 <?php
-  session_start();
+session_start();
 
-  //multilingue
-  if (!isset($_SESSION['lang'])) {
-    $_SESSION['lang'] = "fr";
-  }
-  include "multilingue/multilingue.php";
-  loadLanguageFromSession($_SESSION['lang']);
+//multilingue
+if (!isset($_SESSION['lang'])) {
+  $_SESSION['lang'] = "fr";
+}
+include "multilingue/multilingue.php";
+loadLanguageFromSession($_SESSION['lang']);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+<head>
   <title>Reservation de chauffeur</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -27,150 +27,150 @@
 
 
   <?php
-    include 'includehtml/head.html'; ?>
-  </head>
-  <body>
-    <?php
+  include 'includehtml/head.html'; ?>
+</head>
+<body>
+  <?php
 
 
-    require 'Class/Autoloader.php';
-    App\Autoloader::register();
-    $bdd = new App\Database('rip');
-    $backOffice=3;
-    $navbar = new App\Navbar($backOffice);
-    $navbar->navbar();;
+  require 'Class/Autoloader.php';
+  App\Autoloader::register();
+  $bdd = new App\Database('rip');
+  $backOffice=3;
+  $navbar = new App\Navbar($backOffice);
+  $navbar->navbar();;
 
-    $form = new App\Form(array());
+  $form = new App\Form(array());
 
-    $trajet = unserialize($_SESSION['trajet']);
-    /*
-    if (isset($_POST[""])) {
-      // code...
-    }
-    */
-    ?>
-   <div class="container">
-      <div class="row">
-        <div class="offset-md-2 col-md-8">
-          <div class="card" style="margin:50px 0">
-             <!-- Default panel contents -->
-             <div class="card-header">
-               <h1 class="display-3">Devis</h1>
-               <?php $trajet->showInfosTrajet(); ?>
-               <?php echo "<p>ID de votre trajet: ".$_SESSION["idTrajet"]."</p>" ?>
-             </div>
+  // On recupere l'objet trajet contenant nos infos de trajet depuis la session
+  $trajet = unserialize($_SESSION['trajet']);
+?>
+<div class="container">
+  <div class="row">
+    <div class="offset-md-2 col-md-8">
+      <div class="card" style="margin:50px 0">
 
-             <div class="list-group list-group-flush">
-               <h3 class="display-5 center-block">Services choisi</h3>
-               <p class="center-block">
-               <?php
-               $idServices = $bdd->query('SELECT idService FROM linkServicetrajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
+        <!-- Infos Trajet -->
+        <div class="card-header">
+          <h1 class="display-3">Devis</h1>
+          <?php $trajet->showInfosTrajet(); ?>
+          <?php echo "<p>ID de votre trajet: ".$_SESSION["idTrajet"]."</p>" ?>
+        </div>
 
-               if (empty($idServices)) {
-                 echo "Aucun services selectionnés";
-               }else {
-                 //var_dump($idServices);
-                 foreach ($idServices as $unIdService) {
-                   //var_dump($unIdService);
-                   $service = $bdd->queryOne('SELECT * FROM services WHERE idService='.$unIdService["idService"].'');
-                   $linkService = $bdd->queryOne('SELECT * FROM linkServicetrajet WHERE idService='.$unIdService["idService"].'');
+        <!-- Info choix des services -->
+        <div class="list-group list-group-flush">
+          <h3 class="display-5 center-block">Services choisi</h3>
+          <p class="center-block">
+            <?php
+            //on recupere les id des services choisis sur ce trajet
+            $idServices = $bdd->query('SELECT idService FROM linkServicetrajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
 
-                   //echo $linkService["idAnnexe"];
-                   //var_dump($linkService);
-                   if($unIdService["idService"] == 1){
-                     $infoLinkService = $bdd->queryOne('SELECT * FROM restaurants WHERE idRestaurant='.$linkService["idAnnexe"].'');
-                     $typeEtablissement="Restaurant";
-                   }elseif ($unIdService["idService"] == 7) {
-                     $infoLinkService = $bdd->queryOne('SELECT * FROM hotel WHERE idHotel='.$linkService["idAnnexe"].'');
-                     $typeEtablissement="Hotel";
-                   }elseif ($unIdService["idService"] == 8) {
-                     $infoLinkService = $bdd->queryOne('SELECT * FROM billettourisme WHERE idBillet='.$linkService["idAnnexe"].'');
-                     $typeEtablissement="Billet touristque";
-                   }else {
+            if (empty($idServices)) {
+              echo "Aucun services selectionnés";
+            }else {
+              //on boucle les id des services choisis
+              foreach ($idServices as $unIdService) {
 
-                   }
+                //on recupere les infos du service en fonction de son id
+                $service = $bdd->queryOne('SELECT * FROM services WHERE idService='.$unIdService["idService"].'');
+                $linkService = $bdd->queryOne('SELECT * FROM linkServicetrajet WHERE idService='.$unIdService["idService"].'');
 
+                //choix en fonciton du type de service special
+                if($unIdService["idService"] == 1){
+                  $infoLinkService = $bdd->queryOne('SELECT * FROM restaurants WHERE idRestaurant='.$linkService["idAnnexe"].'');
+                  $typeEtablissement="Restaurant";
+                }elseif ($unIdService["idService"] == 7) {
+                  $infoLinkService = $bdd->queryOne('SELECT * FROM hotel WHERE idHotel='.$linkService["idAnnexe"].'');
+                  $typeEtablissement="Hotel";
+                }elseif ($unIdService["idService"] == 8) {
+                  $infoLinkService = $bdd->queryOne('SELECT * FROM billettourisme WHERE idBillet='.$linkService["idAnnexe"].'');
+                  $typeEtablissement="Billet touristque";
+                }else {
 
-                   if ($linkService["idAnnexe"] < 0) {
-                     echo "ID: ".$service["idService"]." | ".$service["nomService"];
-                   }else{
-                     //var_dump($infoLinkService);
-                     echo "ID: ".$service["idService"]." | ".$service["nomService"]." : ".$typeEtablissement." ".$infoLinkService["nom"]." | Quantitée: ".$linkService["quantite"]." places";
-                   }
-                   echo "<br><br>";
-                 }
+                }
+
+                //Affichage des infos
+                if ($linkService["idAnnexe"] < 0) {
+                  echo "ID: ".$service["idService"]." | ".$service["nomService"];
+                }else{
+                  echo "ID: ".$service["idService"]." | ".$service["nomService"]." : ".$typeEtablissement." ".$infoLinkService["nom"]." | Quantitée: ".$linkService["quantite"]." places";
+                }
+                echo "<br><br>";
               }
+            }
+            ?>
+          </p>
+        </div>
+
+        <!-- Info choix des services -->
+        <div class="list-group-item">
+          <div class="list-group list-group-flush">
+            <h3 class="display-5 center-block">Votre chauffeur</h3>
+            <p>
+              <?php
+              //$idServices = $bdd->query('SELECT idService FROM linkServicetrajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
+              $idChauffeur= $bdd->queryOne('SELECT idChauffeur FROM trajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
+              $unChauffeur= $bdd->queryOne('SELECT * FROM collaborateurs WHERE idCollaborateurs='.$idChauffeur["idChauffeur"].'');
+
+              $car=App\Chauffeur::getCar($unChauffeur["idCollaborateurs"],$bdd);
+              $chauffeur = new App\Chauffeur($unChauffeur["idCollaborateurs"],$unChauffeur["email"],$unChauffeur["last_name"],$unChauffeur["first_name"],$unChauffeur["metier"],$unChauffeur["prixCollaborateur"],
+              $unChauffeur["dateEmbauche"],$unChauffeur["ville"],$unChauffeur["heuresTravailees"],$car["carId"],$car["carBrand"],$car["carModel"],$car["carColor"],$car["nbPlaces"]);
               ?>
-               </p>
-             </div>
-
-             <div class="list-group-item list-group-flush">
-               <div class="">
-                 <h3 class="center-block">Votre chauffeur</h3>
-               </div>
-
-               <p>
-               <?php
-               //$idServices = $bdd->query('SELECT idService FROM linkServicetrajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
-               $idChauffeur= $bdd->queryOne('SELECT idChauffeur FROM trajet WHERE idTrajet='.$_SESSION["idTrajet"].'');
-               $unChauffeur= $bdd->queryOne('SELECT * FROM collaborateurs WHERE idCollaborateurs='.$idChauffeur["idChauffeur"].'');
-
-               $car=App\Chauffeur::getCar($unChauffeur["idCollaborateurs"],$bdd);
-               $chauffeur = new App\Chauffeur($unChauffeur["idCollaborateurs"],$unChauffeur["email"],$unChauffeur["last_name"],$unChauffeur["first_name"],$unChauffeur["metier"],$unChauffeur["prixCollaborateur"],
-                                             $unChauffeur["dateEmbauche"],$unChauffeur["ville"],$unChauffeur["heuresTravailees"],$car["carId"],$car["carBrand"],$car["carModel"],$car["carColor"],$car["nbPlaces"]);
-               ?>
-                <li class="list-group">
-                  <?php //echo $i?>
-                  <h4><?php echo $chauffeur->getFirst_name()." ".$chauffeur->getLast_name();?></h4>
-                  <h6>id du Chauffeur: <?php echo $chauffeur->getIdCollaborateur(); ?> Prix: <?php echo $chauffeur->getPrixCollaborateur()."€ / Km  // AJOUTER SYSTEME PHOTO + SYSTEME ETOILES" ?></h6>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <h4>Infos: </h4>
-                      <ul>
-                        <li><?php echo "Ville d'operation: ".$chauffeur->getVille(); ?></li>
-                        <li><?php echo "Heures Travailées: ".$chauffeur->getHeuresTravailees(); ?></li>
-                        <li><?php echo "Date d'embauche: ".$chauffeur->getDateEmbauche(); ?></li>
-                      </ul>
-                    </div>
-                    <div class="col-md-6">
-                      <h4>Véhicule: </h4>
-                      <ul>
-                        <li><?php echo "Marque: ".$chauffeur->getCarBrand(); ?></li>
-                        <li><?php echo "Modèle: ".$chauffeur->getCarModel(); ?></li>
-                        <li><?php echo "Couleur: ".$chauffeur->getCarColor(); ?></li>
-                      </ul>
-                    </div>
+              <li class="list-group">
+                <?php //echo $i?>
+                <h4><?php echo $chauffeur->getFirst_name()." ".$chauffeur->getLast_name();?></h4>
+                <h6>id du Chauffeur: <?php echo $chauffeur->getIdCollaborateur(); ?> Prix: <?php echo $chauffeur->getPrixCollaborateur()."€ / Km  // AJOUTER SYSTEME PHOTO + SYSTEME ETOILES" ?></h6>
+                <div class="row">
+                  <div class="col-md-6">
+                    <h4>Infos: </h4>
+                    <ul>
+                      <li><?php echo "Ville d'operation: ".$chauffeur->getVille(); ?></li>
+                      <li><?php echo "Heures Travailées: ".$chauffeur->getHeuresTravailees(); ?></li>
+                      <li><?php echo "Date d'embauche: ".$chauffeur->getDateEmbauche(); ?></li>
+                    </ul>
                   </div>
+                  <div class="col-md-6">
+                    <h4>Véhicule: </h4>
+                    <ul>
+                      <li><?php echo "Marque: ".$chauffeur->getCarBrand(); ?></li>
+                      <li><?php echo "Modèle: ".$chauffeur->getCarModel(); ?></li>
+                      <li><?php echo "Couleur: ".$chauffeur->getCarColor(); ?></li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            </p>
+          </div>
+        </div>
 
-                </li>
+        <!-- Recapitulatif des prix -->
+        <div class="list-group-item">
+          <div class="list-group list-group-flush">
+            <h3 class="center-block">Récapitulatif des prix</h3>
+            <p>
+              <ul>
+                <li>Prix chauffeur/trajet: <?php echo $chauffeur->getPrixCollaborateur()." €/Km " ?>* <?php echo $trajet->getDistance()." Km " ?>= <?php echo $trajet->getDistance()*$chauffeur->getPrixCollaborateur()." €" ?></li>
+                <li>Prix Services: </li>
+                <li></li>
+              </ul>
+            </p>
+            <p>
+              Prix total HT :
+            </p>
+          </div>
+        </div>
 
-               </p>
-             </div>
-             <div class="list-group-item list-group-flush">
-               <div class="">
-                 <h3 class="center-block">Récapitulatif des prix</h3>
-               </div>
-               <p>
-                 <ul>
-                   <li>Prix chauffeur/trajet: <?php echo $chauffeur->getPrixCollaborateur()." €/Km " ?>* <?php echo $trajet->getDistance()." Km " ?>= <?php echo $trajet->getDistance()*$chauffeur->getPrixCollaborateur()." €" ?></li>
-                   <li>Prix Services: </li>
-                   <li></li>
-                 </ul>
-               </p>
-               <p>
-                 Prix total HT :
-               </p>
-             </div>
-             <div class="row list-group-item center-block">
-               <div class="center-block">
-                 <?php echo $form->submit(); ?>
-               </div>
-             </div>
+        <!-- Validation -->
+        <div class="row list-group-item center-block">
+          <div class="center-block">
+            <?php echo $form->submit(); ?>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <?php include "includehtml/footer.php" ?>
-  </body>
+<?php include "includehtml/footer.php" ?>
+</body>
 </html>
--- valideReservationServices.php
+++ valideReservationServices.php
@@ -4,57 +4,51 @@ require 'Class/Autoloader.php';
 App\Autoloader::register();
 $bdd = new App\Database('rip');
 
+//verifications que les variables récupérées ne sont pas vides et existent bien
+if (isset($_POST['services']) && (!empty($_POST['services'])) && isset($_POST['quantite']) && (!empty($_POST['quantite']))) {
 
-if (isset($_POST['services']) && (!empty($_POST['services']))) {
   $servicesChoisi=$_POST['services'];
-
   $quantiteCertainService=$_POST['quantite'];
 
-
-
-  echo "<br>spacer<br>";
-  var_dump($_POST['quantite']);
-  var_dump($servicesChoisi);
-
-  if(isset($servicesChoisi)){
-    foreach ($servicesChoisi as $service) {
-
-      foreach ($quantiteCertainService as $key => $quantite) {
-        if ($key == $service) {
-          $thisQuantite=$quantite;
-
-        }
+  //on boucle nos services choisis
+  foreach ($servicesChoisi as $service) {
+    // On recupere la quantite si c'est un service qui a une quantité en foncition de son id
+    foreach ($quantiteCertainService as $key => $quantite) {
+      if ($key == $service) {
+        $thisQuantite=$quantite;
       }
-      switch ($service) {
-        case 1:
-        $idAnnexe=$_POST["idRestaurant"];
-
-        break;
-        case 7:
-        $idAnnexe=$_POST["idHotel"];
-        break;
-        case 8:
-        $idAnnexe=$_POST["idBillet"];
-        break;
-
-        default:
-        $idAnnexe=-1;
-        $thisQuantite=0;
-        break;
-      }
-      echo "aaa ".$thisQuantite;
-
-      $req=$bdd->getPDO()->prepare('INSERT INTO linkServicetrajet (`idTrajet`,`idService`,`idAnnexe`,`quantite`) VALUES (:idTrajet,:idService,:idAnnexe,:quantite)');
-      $req->bindValue(':idTrajet', $_SESSION["idTrajet"]);
-      $req->bindValue(':idService', $service);
-      $req->bindValue(':idAnnexe', $idAnnexe);
-      $req->bindValue(':quantite', $thisQuantite);
-      $req->execute();
-      $req->closeCursor();
-
     }
+
+    //affectation de l'id annexe du service si besoin
+    switch ($service) {
+      case 1:
+      $idAnnexe=$_POST["idRestaurant"];
+      break;
+      case 7:
+      $idAnnexe=$_POST["idHotel"];
+      break;
+      case 8:
+      $idAnnexe=$_POST["idBillet"];
+      break;
+      default:
+      $idAnnexe=-1;
+      $thisQuantite=0;
+      break;
+    }
+    
+    //on insere les id et la quantité pour lier ce choix de service au trajet dans cette table e liaison
+    $req=$bdd->getPDO()->prepare('INSERT INTO linkServicetrajet (`idTrajet`,`idService`,`idAnnexe`,`quantite`) VALUES (:idTrajet,:idService,:idAnnexe,:quantite)');
+    $req->bindValue(':idTrajet', $_SESSION["idTrajet"]);
+    $req->bindValue(':idService', $service);
+    $req->bindValue(':idAnnexe', $idAnnexe);
+    $req->bindValue(':quantite', $thisQuantite);
+    $req->execute();
+    $req->closeCursor();
+
   }
-    header("location: resevationChooseDriver.php");
+
+  //redirection
+  header("location: resevationChooseDriver.php");
 }
 else {
   header("location:resevationChooseService.php?probleme_de_case");

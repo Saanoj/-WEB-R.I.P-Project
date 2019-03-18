1d72ce999aa3ed8c2c5b7147d7c1909383042ece
natin56y <32836662+natin56y@users.noreply.github.com>
2019-03-18 18:29:06 +0100

    fix, collab mis en offline quand il se deco

--- deconnexion.php
+++ deconnexion.php
@@ -1,5 +1,17 @@
 <?php
+require 'Class/Autoloader.php';
+App\Autoloader::register();
+$bdd = new App\Database('rip');
+
 session_start();
+
+if ($_SESSION['isCollaborateur']==1) {
+  $req=$bdd->getPDO()->prepare('UPDATE collaborateurs SET isOnline=:isOnline WHERE idCollaborateurs=:idCollaborateurs');
+  $req->bindValue(':idCollaborateurs', $_SESSION["id"]);
+  $req->bindValue(':isOnline', 0);
+  $req->execute();
+  $req->closeCursor();
+}
 session_destroy();
 header('location: index.php');
 exit;

27845cf9d63d2973e94d6d0832cd938a16be817c
Tutur77 <38217644+Tutur77@users.noreply.github.com>
2019-03-21 15:06:29 +0100

    fix erreurs connexions

--- VerifConnexion.php
+++ VerifConnexion.php
@@ -1,7 +1,14 @@
 <?php
 namespace App;
 use \PDO;
-
+?>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<?php
 
 require 'Class/Autoloader.php';
 Autoloader::register();
@@ -90,20 +97,19 @@ if  (
            }
            else
            {
-            echo '<h4 class = "text-center">Vous avez été banni</h4> <br>
-            <li class="nav-item text-center">
-              <a class="nav-link" href="connexion.php">
-                <i class="fa d-inline fa-lg fa-arrow-circle-left "></i> retour</a>
-            </li> ';
+            ?><div class="alert alert-danger" role="alert">
+            Vous avez été banni ! Veuillez contacter le support afin d'avoir plus d'informations.
+              </div> <?php 
+              header ('Refresh: 3;URL=connexion.php');
+            
            }
         }
            else {
 
-            echo '<h4 class = "text-center">mauvais mot de passe ou pseudo</h4> <br>
-            <li class="nav-item text-center">
-              <a class="nav-link" href="connexion.php">
-                <i class="fa d-inline fa-lg fa-arrow-circle-left "></i> retour</a>
-            </li> ';
+            ?><div class="alert alert-danger" role="alert">
+            Mauvais identifiant ! Veuillez vous reconnecter.
+              </div> <?php
+              header ('Refresh: 3;URL=connexion.php');
 
            }
             }

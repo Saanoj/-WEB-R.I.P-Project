018220dfb806ee3fc8edf8a5057e235c88ac00e7
Saanoj <jonasnizard@gmail.co>
2019-03-24 16:59:49 +0100

    s

--- StatsToExcel/src/statsToExcel/Bdd.java
+++ StatsToExcel/src/statsToExcel/Bdd.java
@@ -8,20 +8,39 @@ public class Bdd {
     public void connect() {
         // JDBC driver name and database URL
         final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        final String DB_URL = "jdbc:mysql://localhost/RIP";
+        final String DB_URL = "jdbc:mysql://localhost/RIP?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
         //  Database credentials
         final String USER = "root";
         final String PASS = "";
         Connection conn = null;
+
         try{
             //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+           // Class.forName("com.mysql.jdbc.Driver");
 
             //STEP 3: Open a connection
             System.out.println("Connecting to a selected database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Connected database successfully...");
+
+
+            Statement stmt = null;
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM users";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                //Retrieve by column name
+                String email  = rs.getString("email");
+
+                //Display values
+                System.out.println("Email: " + email);
+
+            }
+            rs.close();
+
         }catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();

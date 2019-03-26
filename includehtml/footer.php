8b770d11cdab4b94dafa770e9a27a213cefa23a7
Arthur Hallier <tutur.77@hotmail.fr>
2019-03-26 18:18:52 +0100

    d

--- includehtml/footer.php
+++ includehtml/footer.php
@@ -52,13 +52,14 @@
       <!-- footer top -->
       <!-- JS -->
       <!-- JS FILES -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="js/bootstrap.min.js"></script>
       <script src="js/jquery.flexslider-min.js"></script>
       <script src="js/jquery.fancybox.pack.js"></script>
       <script src="js/retina.min.js"></script>
       <script src="js/modernizr.js"></script>
       <script src="js/main.js"></script>
-      <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      

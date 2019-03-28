11336eec0276a1d308a5d802288be1cac0be48ca
Saanoj <jonasnizard@gmail.com>
2019-03-28 15:27:17 +0100

    Java ne le cassez plus :(

--- StatsToExcel/src/statsToExcel/Bdd.java
+++ StatsToExcel/src/statsToExcel/Bdd.java
@@ -2,63 +2,53 @@ package statsToExcel;
 import java.sql.*;
 
 public class Bdd {
-    public Bdd(){
 
+
+    //  Database credentials
+    private String USER;
+    private String PASS;
+    private String DB_URL;
+    private Connection conn = null;
+
+    public Bdd(String user, String pass, String dbName) {
+        this.USER = user;
+        this.PASS = pass;
+        this.DB_URL = "jdbc:mysql://localhost/" + dbName + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     }
-    public void connect() {
-        // JDBC driver name and database URL
-        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        final String DB_URL = "jdbc:mysql://localhost/RIP?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
-        //  Database credentials
-        final String USER = "root";
-        final String PASS = "";
-        Connection conn = null;
+    public Connection getConn() {
+        return conn;
+    }
 
-        try{
-            //STEP 2: Register JDBC driver
-           // Class.forName("com.mysql.jdbc.Driver");
+    public void startConnect() {
+
+        try {
 
             //STEP 3: Open a connection
             System.out.println("Connecting to a selected database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Connected database successfully...");
 
-
-            Statement stmt = null;
-            stmt = conn.createStatement();
-
-            String sql = "SELECT * FROM users";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while(rs.next()){
-                //Retrieve by column name
-                String email  = rs.getString("email");
-
-                //Display values
-                System.out.println("Email: " + email);
-
-            }
-            rs.close();
-
-        }catch(SQLException se){
+        } catch (SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
             System.out.println("Handle errors for JDBC");
-        }catch(Exception e){
+        } catch (Exception e) {
             //Handle errors for Class.forName
             e.printStackTrace();
             System.out.println("Handle errors for Class.forName");
-        }finally{
-            //finally block used to close resources
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//
-}
+        }
 
+    }
+
+    public void stopConnect(Connection conn) {
+        //finally block used to close resources
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }//end finally try
+        System.out.println("Goodbye!");
+    }
+}
--- StatsToExcel/src/statsToExcel/Main.java
+++ StatsToExcel/src/statsToExcel/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
     Bdd bdd = new Bdd("root","","RIP");
     Request rqt = new Request(bdd);
-    ArrayList req = rqt.req("Select * from trajet","last_name");
+    ArrayList req = rqt.req("Select * from users","email");
     System.out.println(req);
     }
 }

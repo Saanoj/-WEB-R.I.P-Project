55e649f54fbd3f43717e34fee0a6e99b8c20e7bb
Saanoj <jonasnizard@gmail.com>
2019-04-09 12:49:15 +0200

    Update sendmail.php

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -6,34 +6,37 @@ include_once "PHPMailer/PHPMailer.php";
 include_once "PHPMailer/Exception.php";
 include_once "PHPMailer/SMTP.php";
 $mail = new PHPMailer();
+define ('GUSER',MAIL);
+define ('GPWD',PASS);
 
-$mail->Host = "ssl://smtp.gmail.com";
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->Username = MAIL;
-$mail->Password = PASS;
-$mail->SMTPSecure = "ssl"; //tls;
-$mail->Port = 465; //587
+smtpmailer("jonasnizard@gmail.com","hallierarthur@gmail.com","Ride in Pride","Votre abbonement","Donnez nous de l'argent");
 
-$mail->addAddress("jonasnizard@gmail.com");
-$mail->setFrom("jonasnizard@gmail.com");
-$mail->Subject = "hello there";
-$mail->isHTML(true);
-$mail->Body = "heloooooooo";
 
-try {
-  $mail->send();
-} catch (\Exception $e) {
-  echo $e;
+// make a separate file and include this file in that. call this function in that file.
+
+function smtpmailer($to, $from, $from_name, $subject, $body) {
+    global $error;
+    $mail = new PHPMailer();  // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 2;  // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true;  // authentication enabled
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+    $mail->SMTPAutoTLS = false;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+
+    $mail->Username = GUSER;
+    $mail->Password = GPWD;
+    $mail->SetFrom($from, $from_name);
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->AddAddress($to);
+    if(!$mail->Send()) {
+        $error = 'Mail error: '.$mail->ErrorInfo;
+        return false;
+    } else {
+        $error = 'Message sent!';
+        return true;
+    }
 }
-
-
-
-if ($mail->send()){
-  echo "Je l'ai envoyez maitre, Igor";
-}else {
-
-  echo $mail->ErrorInfo;
-}
-
  ?>

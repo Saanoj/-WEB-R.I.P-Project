98be1dfa3280e5f253decf3319753837d1fade39
Arthur Hallier <tutur.77@hotmail.fr>
2019-04-09 14:14:01 +0200

    Update sendmail.php

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -9,22 +9,36 @@ $mail = new PHPMailer();
 define ('GUSER',MAIL);
 define ('GPWD',PASS);
 
-smtpmailer("jonasnizard@gmail.com","Votre abbonement","Donnez nous de l'argent");
+$body = "Votre abonnement expire d'ici 1 jours. Si vous voulez continuer a ";
+$body .= "profiter de nos différents services, veuillez re souscrire à un abonnement."; 
+$body .=  '<br>';
+$body .= "Veuillez cliquez";
+$body .= " <a href='http://localhost/-WEB-R.I.P-Project/profil.php'>ici</a>";
+
+if (smtpmailer("rideinprideesgi@gmail.com","Renouvellement d'abonnement.",$body) == true)
+{
+  "Message send ! ";
+}
+else
+{
+  "Error ! ";
+}
+
 
 
 // make a separate file and include this file in that. call this function in that file.
 
-function smtpmailer($to, $from_name, $subject, $body) {
+function smtpmailer($to, $subject, $body) {
     global $error;
     $mail = new PHPMailer();  // create a new object
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 2;  // debugging: 1 = errors and messages, 2 = messages only
+   // $mail->SMTPDebug = 2;  // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth = true;  // authentication enabled
     $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
     $mail->SMTPAutoTLS = false;
     $mail->Host = 'smtp.gmail.com';
     $mail->Port = 587;
 
+    $mail->isHTML(true);
     $mail->Username = GUSER;
     $mail->Password = GPWD;
     $mail->SetFrom("rideinprideesgi@gmail.com", "Ride in Pride");

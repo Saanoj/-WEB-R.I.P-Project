f6403a612f15e1faf8945a9c3129b71a7fb2b907
Tutur77 <38217644+Tutur77@users.noreply.github.com>
2019-04-11 22:27:54 +0200

    Update main.js

--- js/reservationChooseService/main.js
+++ js/reservationChooseService/main.js
@@ -1,88 +1,28 @@
 function displayError(input, message) {
-    input.style.borderColor = 'red';
-    var error = document.createElement('p');
-    error.innerHTML = message;
-    error.style.color = 'red';
-    var parent = input.parentNode;
-    parent.appendChild(error);
+  input.style.borderColor = 'red';
+  var error = document.createElement('p');
+  error.innerHTML = message;
+  error.style.color = 'red';
+  var parent = input.parentNode;
+  parent.appendChild(error);
+}
+
+function clearInput(input) {
+  input.style.borderColor = '';
+
+  var parent = input.parentNode;
+  var elements = parent.getElementsByTagName('p');
+  if(elements.length > 0){
+    parent.removeChild(elements[0]);
   }
-  
-  function clearInput(input) {
-    input.style.borderColor = '';
-  
-    var parent = input.parentNode;
-    var elements = parent.getElementsByTagName('p');
-    if(elements.length > 0){
-      parent.removeChild(elements[0]);
-    }
-  }
+}
 
 
-  function checkHeureFinSportif(heure)
-  {
-      clearInput(heure);
-  
-      hStart = document.getElementById("startCoachSportif").value
-      hStart = hStart.split(":")
-      var dateStart = new Date()
-      dateStart.setHours(hStart[0])
-      dateStart.setMinutes(hStart[1])
-  
-      heures = heure.value;
-      heures = heures.split(":")
-      var dateEnd = new Date()
-      dateEnd.setHours(heures[0])
-      dateEnd.setMinutes(heures[1])
-
-  
-      if (dateEnd.getTime() >= dateStart.getTime())
-  
-  {
-    return true;
-  }
-  else
-  {
-    displayError(heure, '');
-    return false;
-  }
-  }
-
-  function checkHeureFinInterprete(heure)
-  {
-      clearInput(heure);
-  
-      hStart = document.getElementById("startInterprete").value
-      hStart = hStart.split(":")
-      var dateStart = new Date()
-      dateStart.setHours(hStart[0])
-      dateStart.setMinutes(hStart[1])
-  
-      heures = heure.value;
-      heures = heures.split(":")
-      var dateEnd = new Date()
-      dateEnd.setHours(heures[0])
-      dateEnd.setMinutes(heures[1])
-
-  
-      if (dateEnd.getTime() >= dateStart.getTime())
-  
-  {
-    return true;
-  }
-  else
-  {
-    displayError(heure, '');
-    return false;
-  }
-
-  }
-
-
-  function checkHeureFinCulture(heure)
+function checkHeureFinSportif(heure)
 {
     clearInput(heure);
 
-    hStart = document.getElementById("startCoachCulture").value
+    hStart = document.getElementById("startCoachSportif").value
     hStart = hStart.split(":")
     var dateStart = new Date()
     dateStart.setHours(hStart[0])
@@ -106,35 +46,115 @@ else
   return false;
 }
 }
-function checkGlobal(donnees)
 
+function checkHeureFinInterprete(heure)
 {
-  var interprete = checkHeureFinInterprete(donnees.heure);
-  var sportif = checkHeureFinSportif(donnees.heure);
-  var culture = checkHeureFinCulture(donnees.heure);
+    clearInput(heure);
+  
 
-  console.log(interprete)
-  console.log(sportif)
-  console.log(culture)
+    hStart = document.getElementById("startInterprete").value
+    hStart = hStart.split(":")
+    var dateStart = new Date()
+    dateStart.setHours(hStart[0])
+    dateStart.setMinutes(hStart[1])
 
-  if (interprete && sportif && culture)
+    heures = heure.value;
+    heures = heures.split(":")
+    console.log(heure);
+    var dateEnd = new Date()
+    dateEnd.setHours(heures[0])
+    dateEnd.setMinutes(heures[1])
+    if (dateEnd.getTime() >= dateStart.getTime() && dateEnd.getTime() < dateStart.getTime() + 28800000)
+{
+  return true;
+}
+else
+{
+  displayError(heure, '');
+  return false;
+}
+}
+
+function checkHeureDebutInterprete(heure) {
+  clearInput(heure);
+  var hourTrajet = document.getElementById("hour").innerHTML
+  hourTrajet = hourTrajet.split(" ")
+  hourTrajet = hourTrajet[1]
+  hourTrajet = hourTrajet.split(":")
+
+  var dateTrajet = new Date()
+  dateTrajet.setHours(hourTrajet[0])
+  dateTrajet.setMinutes(hourTrajet[1])
+
+  var hStart = document.getElementById("startInterprete").value
+  hStart = hStart.split(":")
+  var dateStart = new Date()
+  dateStart.setHours(hStart[0])
+
+
+  console.log( dateTrajet.getTime())
+
+  if (dateStart.getTime() > dateTrajet.getTime() )
   {
     return true;
   }
-  else {
+  else
+  {
+    displayError(heure, '');
     return false;
   }
 }
 
-$(document).ready(function(){
-  $('.count').prop('disabled', true);
-   $(document).on('click','.plus',function(){
-  $('.count').val(parseInt($('.count').val()) + 1 );
-  });
-    $(document).on('click','.minus',function(){
-    $('.count').val(parseInt($('.count').val()) - 1 );
-      if ($('.count').val() == 0) {
-      $('.count').val(1);
-    }
-      });
-});
+
+
+
+function checkHeureFinCulture(heure)
+{
+  clearInput(heure);
+
+  hStart = document.getElementById("startCoachCulture").value
+  hStart = hStart.split(":")
+  var dateStart = new Date()
+  dateStart.setHours(hStart[0])
+  dateStart.setMinutes(hStart[1])
+
+  heures = heure.value;
+  heures = heures.split(":")
+ 
+  var dateEnd = new Date()
+
+  dateEnd.setHours(heures[0])
+  dateEnd.setMinutes(heures[1])
+
+
+  if (dateEnd.getTime() >= dateStart.getTime())
+
+{
+return true;
+}
+else
+{
+displayError(heure, '');
+return false;
+}
+}
+function checkGlobal(donnees)
+
+{
+var interprete = checkHeureFinInterprete(donnees.heure);
+var interpreteDebut = checkHeureDebutInterprete(donnees.heure);
+var sportif = checkHeureFinSportif(donnees.heure);
+var culture = checkHeureFinCulture(donnees.heure);
+
+console.log(interprete)
+console.log(sportif)
+console.log(culture)
+
+if (interprete && sportif && culture && interpreteDebut)
+{
+  return true;
+}
+else {
+  return false;
+}
+}

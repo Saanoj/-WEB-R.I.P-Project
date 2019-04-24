669b93b0d8933d2faf630872ff2185cd44ba3eb9
natin56y <32836662+natin56y@users.noreply.github.com>
2019-04-24 14:10:02 +0200

    update api fini sauf fin des trajets

--- API/api/users/putTripValidated.php
+++ API/api/users/putTripValidated.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 require_once __DIR__ . '/../../utils/DatabaseManager.php';
 require_once __DIR__ . '/../../services/TrajetService.php';
 
-$collab = TrajetService::getInstance()->setTripValidated($_GET["idTrajet"],$_GET["metier"],$_GET["idCollab"]);
-echo '{"id":'.$_GET["idTrajet"].',"isValidated":1}';
+$ret = TrajetService::getInstance()->setTripValidated($_GET["idTrajet"],$_GET["metier"],$_GET["idCollab"]);
+echo '{"id":'.$_GET["idTrajet"].',"isValidated":'.$ret.'}';
 
  ?>

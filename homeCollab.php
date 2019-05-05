db0f8b689e217126859b8d5060e8f0529e3d8813
natin56y <32836662+natin56y@users.noreply.github.com>
2019-05-06 01:55:46 +0200

    Update homeCollab.php

--- homeCollab.php
+++ homeCollab.php
@@ -238,19 +238,19 @@ session_start();
       </div>
       <div class="dropdown-divider"></div>
       <h3>Mes rémunérations</h3>
-      <div class="row m-5">
+      <div class="container m-5">
 
         <?php
         $idCollaborateur = $_SESSION["id"];
 
         $list = $bdd->query('SELECT * FROM remuneration WHERE idCollab = '.$idCollaborateur.'');
 
-        if (empty($trips)) {
+        if (empty($list)) {
           echo "Aucun trajets dans cette liste";
         } else {
           foreach ($list as $item) {
             $trip = $bdd->queryOne('SELECT * FROM trajet WHERE idtrajet = '.$item["idTrajet"].'');
-            echo "<p>Tajet num: ".$trip["idTrajet"]." le ".$trip["heureDebut"]." pour un prix de ".$item["Price"]." €</p>";
+            echo "<p class='row'><strong>Tajet </strong>: id-".$trip["idTrajet"]." le ".$trip["heureDebut"]." pour un prix de ".$item["Price"]." € </p>\n";
           }
         }
          ?>
